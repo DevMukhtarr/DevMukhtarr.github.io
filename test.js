c55d779e92be8d824b86d835737c9e662a52546d
Your Name <you@example.com>
2021-02-16 17:11:07 +0100

    i added my new project and also improved my portfolio's homepage

--- test.js
+++ test.js
@@ -1,18 +1,14 @@
 let menu = document.querySelector('.menu--items--mobile');
-let ball = document.querySelector('#ball');
-let text = document.querySelector('.my-intro');
 let intro = "Hello, I am Abdulfatai Mukhtar a software developer.";
 let introDiv = document.querySelector('.my-intro');
+let bar = document.querySelector('.fa-bars')
 let i = 0;
+let body = document.querySelector('.body--about')
 
-toggle.addEventListener('click', () =>{
+bar.addEventListener('click', () =>{
     menu.classList.toggle('active');
 })
-ball.onclick = () =>{
-    document.body.classList.toggle('dark');
-    ball.classList.toggle('move');
-    text.classList.toggle('color-change');
-}
+
 typerForIntro = () =>{
     if(i < intro.length){
         introDiv.innerHTML += intro.charAt(i);
